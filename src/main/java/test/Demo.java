c61725f6e17a0c37721e2b80007289d9ecfb098b
daixiongkun <daixiongkun@1ziton.com>
2020-11-15 22:05:02 +0800

    初始化练习2

--- src/main/java/test/Demo.java
+++ src/main/java/test/Demo.java
@@ -1,4 +1,42 @@
 package test;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Demo {
+    public static void main(String[] args) {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("http://www.baidu.com");
+        CloseableHttpResponse response = null;
+        try {
+            response = httpClient.execute(httpGet);
+            if (response.getStatusLine().getStatusCode() == 200){
+                String content = EntityUtils.toString(response.getEntity(),"utf-8");
+                FileUtils.writeStringToFile(new File("D:\\gljj\\baidu.html"),content,"utf-8");
+                System.out.println("内容长度："+content.length());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if (response != null){
+                try {
+                    response.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    httpClient.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
